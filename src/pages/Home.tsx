d0608c732bb8aedd8e595d4b17ff5cde6f348b3d
alexkgiang <104709844+alexkgiang@users.noreply.github.com>
2023-03-16 23:11:25 -0400

    Fix home page formatting

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+import bear from './images/bear.png';
+import styles from './styles/home.module.css';
+
+function Home() {
+  return (
+    <main className={styles.homecontainer}>
+      <div className={styles.homeimage}>
+        <Image
+          src={bear}
+          alt="cusb bear"
+          priority
+        />
+      </div>
+      <div className={styles.hometext}>
+        <h1 className={styles.homeheader}>Welcome to Cornell University's Project Team</h1>
+        <p className={styles.desc}>Founded in 2017, our team has aimed to design and build an innovative, single-occupant, solar-powered boat to compete in the annual Solar Splash competition.</p>
+      </div>
+    </main>
+  )
+}
+
+export default Home;
