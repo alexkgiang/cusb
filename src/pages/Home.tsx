32db1ec55e1daf52b14d716d12f2ee9f55972765
alexkgiang <104709844+alexkgiang@users.noreply.github.com>
2023-04-13 23:58:32 -0400

    Update Home Text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,7 +18,7 @@ function Home() {
       </div>
       <div className={styles.hometext}>
         <h1 className={styles.homeheader}>Welcome to Cornell University's <span style={{ color: "#FC0C0C" }} >Solar Boat</span> Project Team</h1>
-        <p className={styles.homedesc}>Founded in 2017, our team has aimed to design and build an innovative, single-occupant, solar-powered boat to compete in the annual Solar Splash competition.</p>
+        <p className={styles.homedesc}>Founded in 2017, our team has aimed to design and build an innovative, single-occupant, 14-foot solar-powered boat to compete in the annual Solar Splash competition.</p>
       </div>
       <Footer />
     </main>
