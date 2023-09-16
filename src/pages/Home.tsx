c3b4f1904fa9a118b0982e0fd1e4292755d0faee
tsangha03 <tsangha03@gmail.com>
2023-09-16 16:35:08 -0400

    Fixed footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,24 +5,32 @@ import styles from './styles/home.module.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
+
 function Home() {
   return (
-    <main className={styles.homecontainer}>
+    <div className={styles.wrapper}>
       <Navbar />
-      <div className={styles.homeimage}>
-        <Image
-          src={bear}
-          alt="cusb bear"
-          priority
-        />
-      </div>
-      <div className={styles.hometext}>
-        <h1 className={styles.homeheader}>Welcome to Cornell University's <span style={{ color: "#FC0C0C" }} >Solar Boat</span> Project Team</h1>
-        <p className={styles.homedesc}>Founded in 2017, our team has aimed to design and build an innovative, single-occupant, 14-foot solar-powered boat to compete in the annual Solar Splash competition.</p>
+      <div className={styles.content}>
+        <div className={styles.hometext}>
+          <h1 className={styles.homeheader}>
+            Welcome to Cornell University's <span style={{ color: "#FC0C0C" }}>Solar Boat</span> Project Team
+          </h1>
+          <p className={styles.homedesc}>
+            Founded in 2017, our team has aimed to design and build an innovative, single-occupant, 14-foot solar-powered boat to compete in the annual Solar Splash competition.
+          </p>
+        </div>
+        <div className={styles.homeimage}>
+          <Image
+            src={bear}
+            alt="cusb bear"
+            priority
+          />
+        </div>
       </div>
       <Footer />
-    </main>
+    </div>
   )
 }
 
 export default Home;
+
